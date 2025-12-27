efc2212eaf76715ded374fe2b8b2128557ee29af
Ananya Sharma <ananyasharma242004@gmail.com>
2025-12-28 05:04:25 +0700

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Creating directories 
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/_init__.py
+touch research/_init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+echo "Directories and files created successfully."
